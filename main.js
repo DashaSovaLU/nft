16c2a310fafec7afe2c1e10fd57c23d0acc9b025
Dasha <johndoe@example.com>
2023-03-18 14:56:36 +0300

    --

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let flylink = document.querySelector("#slider__top");
 
+const swiper = new Swiper(flylink, {
+	// Optional parameters
+	loop: true,
+	slidesPerView: "auto",
+	spaceBetween: 22,
 
-console.log('qqq');
+	navigation: {
+		nextEl: ".top__arrow._right",
+		prevEl: ".top__arrow._left",
+	},
+});
