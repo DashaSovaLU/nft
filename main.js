19ae15a9833ebeccec38d9f81a4bd1705c38c338
Dasha <johndoe@example.com>
2023-03-26 18:27:25 +0300

    cons

--- main.js
+++ main.js
@@ -12,11 +12,48 @@ const swiper = new Swiper(flylink, {
 	},
 });
 
-const heartLikeList = document.querySelectorAll(".heart__like");
-console.log(heartLikeList);
+const heartLikeList = document.querySelectorAll("[data-btn-like]");
 
-for (const iterator of heartLikeList) {
-	iterator.addEventListener("click", () => {
-		iterator.classList.toggle("_active");
+for (const btn of heartLikeList) {
+	let number = +btn.dataset.btnLike;
+	btn.innerHTML = number;
+
+	btn.addEventListener("click", () => {
+		if (btn.classList.contains("_active")) {
+			number = number - 1;
+		} else {
+			number = number + 1;
+		}
+		btn.innerHTML = number;
+
+		btn.classList.toggle("_active");
 	});
 }
+
+// значения по умолчанию(старославянский)
+function likeHuike(number, humber = 10) {
+	const result = humber + number;
+	console.log(result);
+}
+// likeHuike(5, 8);
+// likeHuike(7);
+
+// современный синтаксис
+const justHui = (number, humber = 10) => {
+	console.log(number + humber);
+};
+// justHui(25, 6);
+// justHui(6);
+
+// функция возвращает результат
+const prostoVagina = (chislo) => {
+	const result = chislo - 2;
+	return result;
+};
+// console.log(prostoVagina(12));
+
+// переменна, которую можно  переопределять
+let prima = "pkok";
+// console.log(prima);
+prima = 48;
+// console.log(prima);
